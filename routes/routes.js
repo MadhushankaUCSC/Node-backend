9443be295c6485730f8505f741c28f0f74019c15
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2021-05-30 16:55:46 +0530

    update routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var productController = require('../src/products/productController');
+var userController = require('../src/user/userController');
 
 var router = express.Router();
 
 
+//user routes
+router.route('/user/create')
+    .post(userController.createUser);
 
+//user login routes
+router.route('/user/login')
+    .post(userController.loginUser);
 
-
-
+//products routes
 router.route('/product/allproducts')
     .get(productController.getAllProducts);
 
